e11ed68f875bfbdd7402dee64263d6030c12c0c5
abdessamieaksas <127380411+abdessamieaksas@users.noreply.github.com>
2025-03-04 16:54:17 +0000

    style overflow

--- resources/js/Layouts/DashboardLayout.jsx
+++ resources/js/Layouts/DashboardLayout.jsx
@@ -6,7 +6,7 @@ import { Link,usePage } from '@inertiajs/react'; // Use Inertia's Link component
 export default function DashboardLayout({ children }) {
    const user = usePage().props.auth.user;
   return (
-    <div className="h-screen flex">
+    <div className="flex">
       {/* LEFT - Sidebar */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/admin" className="flex items-center justify-center lg:justify-start gap-2">
@@ -17,7 +17,7 @@ export default function DashboardLayout({ children }) {
       </div>
 
       {/* RIGHT - Main Content */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]  flex flex-col">
         <Navbar auth={user} />
         {children} {/* This will render the page content */}
       </div>
